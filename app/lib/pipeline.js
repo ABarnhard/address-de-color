6b5cce12d7590079de472aeafe9ccf70bee68711
Adam Barnhard <admbarn@gmail.com>
2014-08-01 12:02:59 -0500

    finished refactor

--- app/lib/pipeline.js
+++ app/lib/pipeline.js
@@ -2,15 +2,14 @@
 
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
+var home = require('../controllers/home');
 
 module.exports = function(app, express){
   app.use(morgan('dev'));
   app.use(express.static(__dirname + '/../static'));
   app.use(bodyParser.urlencoded({extended:true}));
 
-  app.get('/', function(req, res){
-    res.render('index');
-  });
+  app.get('/', home.index);
 
   console.log('Express Pipeline Established');
 };
